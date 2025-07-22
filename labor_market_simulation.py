67df8aea7f866530666ef19ee9b71c9e34f620af
Serafina Kamp <serafibk@serafina-mba.local>
2025-07-22 16:52:43 -0400

    simulation outline

---
+++ labor_market_simulation.py
@@ -0,0 +1,57 @@
+# imports
+
+
+# functions (which can be moved to a separate file)
+
+def worker_initialization():
+    #todo
+    continue
+
+def firm_initialization():
+    #todo
+    continue
+
+def worker_information_seeking():
+    # todo
+    continue
+
+def firm_information_seeking():
+    # todo
+    continue
+
+def negotiation()
+    #todo
+    continue
+
+def worker_belief_update()
+    #todo
+    continue
+
+def firm_belief_update()
+    #todo
+    continue
+
+def mvpt_update()
+    #todo
+    continue
+
+def market_time_step():
+    # todo
+    continue
+
+def check_for_convergence()
+    #todo
+    continue
+
+
+if __name__ == "__main__":
+
+    # labor market parameters
+
+
+
+
+    # run simulation
+
+
+    # analyze results 
